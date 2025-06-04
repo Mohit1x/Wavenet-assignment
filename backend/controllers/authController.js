3f0432cc2cea0dffb3ab4912c5564fdde2cc35ef
mohit1x <mohitvyas746@gmail.com>
2025-06-04 22:34:00 +0530

    build fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -77,8 +77,8 @@ const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: false,
+        sameSite: "lax",
         maxAge: 24 * 60 * 60 * 1000,
       })
       .status(200)
